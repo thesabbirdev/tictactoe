7d0da252bae1342524110ea5c78a2820d06e5c71
thesabbirdev <sabbir26.dev@gmail.com>
2024-05-24 12:09:23 +0600

    first commit

---
+++ tictactow/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import TicTacTae from './Components/TicTacToe/TicTacToe'
+
+function App() {
+ 
+
+  return (
+    <>
+     <TicTacTae/>
+    </>
+  )
+}
+
+export default App
